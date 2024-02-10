ed904d6b9c8e39ad6c9579ab952e4dbd5526f5aa
NEXZ <76069669+soynexz@users.noreply.github.com>
2024-02-11 00:13:34 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 // Copyright (c) Toolbi Software. All rights reserved.
 // Check the README file in the project root for more information.
 
+// TODO Automate the releases.
+
 mod modules;
 mod tests;
 

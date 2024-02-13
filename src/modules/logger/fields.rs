194e107568589fde8637bf65c6f9647d008f50bf
NEXZ <76069669+soynexz@users.noreply.github.com>
2024-02-13 13:58:17 +0100

    Create fields.rs

---
+++ src/modules/logger/fields.rs
@@ -0,0 +1,33 @@
+// Copyright (c) Toolbi Software. All rights reserved.
+// Check the README file in the project root for more information.
+
+#[derive(Debug)]
+pub struct LogFields {
+  category: Option<String>,
+  message: Option<String>,
+  ms: Option<f64>,
+}
+
+impl tracing::field::Visit for LogFields {
+  fn record_debug(&mut self, field: &tracing::field::Field, value: &dyn std::fmt::Debug) {
+    match field.name() {
+      "message" => self.message = Some(format!("{:?}", value)),
+      _ => {}
+    }
+  }
+
+  fn record_str(&mut self, field: &tracing::field::Field, value: &str) {
+    match field.name() {
+      "category" => self.category = Some(value.into()),
+      "message" => self.message = Some(value.into()),
+      _ => {}
+    }
+  }
+
+  fn record_f64(&mut self, field: &tracing::field::Field, value: f64) {
+    match field.name() {
+      "ms" => self.ms = Some(value),
+      _ => {}
+    }
+  }
+}

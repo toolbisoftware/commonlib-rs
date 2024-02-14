1fb35cd538171a7ce5e524cef958c1aaacc7e273
NEXZ <76069669+soynexz@users.noreply.github.com>
2024-02-14 13:24:10 +0100

    Create pad_strings_to_length.rs

---
+++ src/modules/pad_strings_to_length.rs
@@ -0,0 +1,27 @@
+// Copyright (c) Toolbi Software. All rights reserved.
+// Check the README file in the project root for more information.
+
+use std::collections::HashMap;
+
+pub fn pad_strings_to_length<K>(strings: HashMap<K, String>, length: usize) -> HashMap<K, String>
+where
+  K: std::hash::Hash + Clone + Eq,
+{
+  let mut hashmap: HashMap<K, String> = HashMap::new();
+
+  for (key, value) in strings.iter() {
+    let value_length: usize = value.len();
+    let string: String = match value_length {
+      len if len > length => value.chars().take(length).collect(),
+      len if len < length => {
+        let padding: usize = length - value_length;
+        format!("{}{}", value, " ".repeat(padding))
+      }
+      _ => value.to_owned(),
+    };
+
+    hashmap.insert(key.clone(), string);
+  }
+
+  hashmap
+}

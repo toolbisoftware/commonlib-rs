4bd7a7333e54d4bf1d31df9a203a4bba92a55792
NEXZ <76069669+soynexz@users.noreply.github.com>
2024-02-14 13:30:07 +0100

    Update pad_string_to_length.rs

--- src/modules/pad_string_to_length.rs
+++ src/modules/pad_string_to_length.rs
@@ -2,11 +2,11 @@
 // Check the README file in the project root for more information.
 
 pub fn pad_string_to_length(string: String, length: usize) -> String {
-  let string_length = string.len();
-  let string = match string_length {
+  let string_length: usize = string.len();
+  let string: String = match string_length {
     len if len > length => string.chars().take(length).collect(),
     len if len < length => {
-      let padding = length - string_length;
+      let padding: usize = length - string_length;
       format!("{}{}", string, " ".repeat(padding))
     }
     _ => string.to_owned(),

7da677dcb5e6dac16309f7135d95777078624b2f
NEXZ <76069669+soynexz@users.noreply.github.com>
2024-02-20 17:58:13 +0100

    Update pad_string_to_length.rs

--- src/modules/pad_string_to_length.rs
+++ src/modules/pad_string_to_length.rs
@@ -1,7 +1,7 @@
 // Copyright (c) Toolbi Software. All rights reserved.
 // Check the README file in the project root for more information.
 
-pub fn pad_string_to_length(string: String, length: usize) -> String {
+pub fn pad_string_to_length(string: &str, length: usize) -> String {
   let string_length: usize = string.len();
   let string: String = match string_length {
     len if len > length => string.chars().take(length).collect(),
@@ -9,7 +9,7 @@ pub fn pad_string_to_length(string: String, length: usize) -> String {
       let padding: usize = length - string_length;
       format!("{}{}", string, " ".repeat(padding))
     }
-    _ => string,
+    _ => string.into(),
   };
 
   string

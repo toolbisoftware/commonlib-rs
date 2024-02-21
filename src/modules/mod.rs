1eeacd81a17e324d682b986735b4deb5362fe8eb
NEXZ <76069669+soynexz@users.noreply.github.com>
2024-02-21 17:12:14 +0100

    Update mod.rs

--- src/modules/mod.rs
+++ src/modules/mod.rs
@@ -1,6 +1,16 @@
 // Copyright (c) Toolbi Software. All rights reserved.
 // Check the README file in the project root for more information.
 
-mod logger;
+#[cfg(feature = "logger")]
+pub mod logger;
+mod pad_string_to_length;
+mod pad_strings_to_length;
+mod pad_strings_to_longest;
+mod stopwatch;
 
+#[cfg(feature = "logger")]
 pub use logger::Logger;
+pub use pad_string_to_length::pad_string_to_length;
+pub use pad_strings_to_length::pad_strings_to_length;
+pub use pad_strings_to_longest::pad_strings_to_longest;
+pub use stopwatch::Stopwatch;

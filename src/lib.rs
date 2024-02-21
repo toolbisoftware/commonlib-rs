45fc2977c6e44aaae4005b124a2e0d715a1afebf
NEXZ <76069669+soynexz@users.noreply.github.com>
2024-02-21 17:13:21 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@
 // Check the README file in the project root for more information.
 
 // TODO Automate the releases.
+// TODO Write in-code documentation.
+// TODO Write tests.
 
 mod error;
 mod modules;

35e958a0548ef017236e2676ae66c50285feb6d4
NEXZ <76069669+soynexz@users.noreply.github.com>
2024-03-22 16:45:06 +0100

    Update mod.rs

--- modules/commonlib/src/str/mod.rs
+++ modules/commonlib/src/str/mod.rs
@@ -1,4 +1,8 @@
 // Copyright (c) Toolbi Software. All rights reserved.
 // Check the README file in the project root for more information.
 
-pub mod pad_to_len;
+mod pad_eq;
+mod pad_len;
+
+pub use pad_eq::pad_eq;
+pub use pad_len::pad_len;

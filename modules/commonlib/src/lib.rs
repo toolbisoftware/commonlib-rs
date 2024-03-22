5c447f4eaf98376e6e031e880afd5b883144e1e3
NEXZ <76069669+soynexz@users.noreply.github.com>
2024-03-22 17:21:22 +0100

    Update lib.rs

--- modules/commonlib/src/lib.rs
+++ modules/commonlib/src/lib.rs
@@ -1,4 +1,7 @@
 // Copyright (c) Toolbi Software. All rights reserved.
 // Check the README file in the project root for more information.
 
+mod stopwatch;
 pub mod str;
+
+pub use stopwatch::{stopwatch, Stopwatch};

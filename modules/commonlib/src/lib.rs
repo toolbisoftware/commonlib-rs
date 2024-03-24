2d8d4b01f5034c3118b1a9cc155e53f01ad8dd74
NEXZ <76069669+soynexz@users.noreply.github.com>
2024-03-24 18:33:01 +0100

    Update lib.rs

--- modules/commonlib/src/lib.rs
+++ modules/commonlib/src/lib.rs
@@ -1,7 +1,12 @@
 // Copyright (c) Toolbi Software. All rights reserved.
 // Check the README file in the project root for more information.
 
+pub mod error;
+#[cfg(feature = "logger")]
+mod logger;
 mod stopwatch;
 pub mod str;
 
+#[cfg(feature = "logger")]
+pub use logger::Logger;
 pub use stopwatch::{stopwatch, Stopwatch};

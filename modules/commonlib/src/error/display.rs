157120a97e8961f3c9576d8cec6a082e28c043f2
NEXZ <76069669+soynexz@users.noreply.github.com>
2024-05-01 19:26:32 +0200

    Create display.rs

---
+++ modules/commonlib/src/error/display.rs
@@ -0,0 +1,27 @@
+// Copyright (c) Toolbi Software. All rights reserved.
+// Check the README file in the project root for more information.
+
+use super::{
+  nester::{create_line, nester},
+  ErrorDisplay,
+};
+
+impl<'a> std::fmt::Display for ErrorDisplay<'a> {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    let category: String = self
+      .0
+      .category
+      .map(|category: &str| format!("[{}] ", category.to_uppercase()))
+      .unwrap_or("".to_string());
+    let line: String = create_line(&format!("{}{}", category, self.0.message), 0);
+    let line_break = self.0.error.is_some().then(|| "\n").unwrap_or("");
+
+    write!(f, "{}{}", line, line_break)?;
+
+    if let Some(ref error) = self.0.error {
+      nester(f, error)?;
+    }
+
+    Ok(())
+  }
+}

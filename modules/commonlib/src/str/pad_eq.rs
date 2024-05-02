c43892b6b502666a9c68d33eb2abba192148b72f
NEXZ <76069669+soynexz@users.noreply.github.com>
2024-05-02 12:45:38 +0200

    Update pad_eq.rs

--- modules/commonlib/src/str/pad_eq.rs
+++ modules/commonlib/src/str/pad_eq.rs
@@ -3,17 +3,37 @@
 
 use std::{collections::HashMap, hash::Hash};
 
-fn run(string: &str, length: usize) -> String {
-  let str_len: usize = string.len();
-  match str_len {
-    str_len if str_len < length => format!("{}{}", string, " ".repeat(length - str_len)),
-    _ => string.to_string(),
-  }
-}
-
 pub trait PadEq {
   type Output;
   fn run(&self) -> Self::Output;
+}
+
+pub fn pad_eq<T: PadEq>(value: T) -> T::Output {
+  value.run()
+}
+
+impl<'a> PadEq for Vec<&'a str> {
+  type Output = Vec<String>;
+  fn run(&self) -> Self::Output {
+    let length: usize = self.iter().map(|v: &&str| v.len()).max().unwrap_or(0);
+    let mut result: Vec<String> = Vec::new();
+    for value in self {
+      result.push(run(value, length))
+    }
+    result
+  }
+}
+
+impl PadEq for Vec<String> {
+  type Output = Vec<String>;
+  fn run(&self) -> Self::Output {
+    let length: usize = self.iter().map(|v: &String| v.len()).max().unwrap_or(0);
+    let mut result: Vec<String> = Vec::new();
+    for value in self {
+      result.push(run(value, length))
+    }
+    result
+  }
 }
 
 impl<'a, K> PadEq for HashMap<K, &'a str>
@@ -24,27 +44,32 @@ where
   fn run(&self) -> Self::Output {
     let length: usize = self.iter().map(|(_, v)| v.len()).max().unwrap_or(0);
     let mut result: HashMap<K, String> = HashMap::new();
-    for (key, string) in self {
-      result.insert(key.to_owned(), run(string, length));
+    for (key, value) in self {
+      result.insert(key.to_owned(), run(value, length));
     }
-
     result
   }
 }
 
-impl<'a> PadEq for Vec<&'a str> {
-  type Output = Vec<String>;
+impl<K> PadEq for HashMap<K, String>
+where
+  K: Hash + Eq + Clone,
+{
+  type Output = HashMap<K, String>;
   fn run(&self) -> Self::Output {
-    let length: usize = self.iter().map(|s| s.len()).max().unwrap_or(0);
-    let mut result: Vec<String> = Vec::new();
-    for string in self {
-      result.push(run(string, length));
+    let length: usize = self.iter().map(|(_, v)| v.len()).max().unwrap_or(0);
+    let mut result: HashMap<K, String> = HashMap::new();
+    for (key, value) in self {
+      result.insert(key.to_owned(), run(value, length));
     }
-
     result
   }
 }
 
-pub fn pad_eq<T: PadEq>(value: T) -> T::Output {
-  value.run()
+fn run(string: &str, length: usize) -> String {
+  let str_len: usize = string.len();
+  match str_len {
+    str_len if str_len < length => format!("{}{}", string, " ".repeat(length - str_len)),
+    _ => string.to_string(),
+  }
 }

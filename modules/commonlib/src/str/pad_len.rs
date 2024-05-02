9307f5e20f6854f2be59c82a4543e1608e93f0ac
NEXZ <76069669+soynexz@users.noreply.github.com>
2024-05-02 12:29:04 +0200

    Update pad_len.rs

--- modules/commonlib/src/str/pad_len.rs
+++ modules/commonlib/src/str/pad_len.rs
@@ -3,24 +3,48 @@
 
 use std::{collections::HashMap, hash::Hash};
 
-fn run(string: &str, length: usize) -> String {
-  let str_len: usize = string.len();
-  match str_len {
-    str_len if str_len > length => string.chars().take(length).collect(),
-    str_len if str_len < length => format!("{}{}", string, " ".repeat(length - str_len)),
-    _ => string.to_string(),
-  }
-}
-
 pub trait PadLen {
   type Output;
   fn run(&self, length: usize) -> Self::Output;
 }
 
+pub fn pad_len<T: PadLen>(value: T, length: usize) -> T::Output {
+  value.run(length)
+}
+
 impl PadLen for &str {
   type Output = String;
   fn run(&self, length: usize) -> Self::Output {
-    run(self, length)
+    string(self, length)
+  }
+}
+
+impl PadLen for String {
+  type Output = String;
+  fn run(&self, length: usize) -> Self::Output {
+    string(self, length)
+  }
+}
+
+impl<'a> PadLen for Vec<&'a str> {
+  type Output = Vec<String>;
+  fn run(&self, length: usize) -> Self::Output {
+    let mut result: Vec<String> = Vec::new();
+    for value in self {
+      result.push(string(value, length))
+    }
+    result
+  }
+}
+
+impl PadLen for Vec<String> {
+  type Output = Vec<String>;
+  fn run(&self, length: usize) -> Self::Output {
+    let mut result: Vec<String> = Vec::new();
+    for value in self {
+      result.push(string(value, length))
+    }
+    result
   }
 }
 
@@ -31,26 +55,32 @@ where
   type Output = HashMap<K, String>;
   fn run(&self, length: usize) -> Self::Output {
     let mut result: HashMap<K, String> = HashMap::new();
-    for (key, string) in self {
-      result.insert(key.to_owned(), run(string, length));
+    for (key, value) in self {
+      result.insert(key.to_owned(), string(value, length));
     }
-
     result
   }
 }
 
-impl<'a> PadLen for Vec<&'a str> {
-  type Output = Vec<String>;
+impl<K> PadLen for HashMap<K, String>
+where
+  K: Hash + Eq + Clone,
+{
+  type Output = HashMap<K, String>;
   fn run(&self, length: usize) -> Self::Output {
-    let mut result: Vec<String> = Vec::new();
-    for string in self {
-      result.push(run(string, length));
+    let mut result: HashMap<K, String> = HashMap::new();
+    for (key, value) in self {
+      result.insert(key.to_owned(), string(value, length));
     }
-
     result
   }
 }
 
-pub fn pad_len<T: PadLen>(value: T, length: usize) -> T::Output {
-  value.run(length)
+fn string(string: &str, length: usize) -> String {
+  let str_len: usize = string.len();
+  match str_len {
+    str_len if str_len > length => string.chars().take(length).collect(),
+    str_len if str_len < length => format!("{}{}", string, " ".repeat(length - str_len)),
+    _ => string.to_string(),
+  }
 }

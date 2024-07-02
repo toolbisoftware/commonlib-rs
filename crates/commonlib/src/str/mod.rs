0e1672b7c2d77611b00c1cfcb1d308aed9c515d4
NEXZ <76069669+soynexz@users.noreply.github.com>
2024-07-02 17:21:46 +0200

    Create mod.rs

---
+++ crates/commonlib/src/str/mod.rs
@@ -0,0 +1,8 @@
+// Copyright (c) Toolbi Software. All rights reserved.
+// Check the README file in the project root for more information.
+
+pub use self::pad_eq::{pad_eq, PadEq};
+pub use self::pad_len::{pad_len, PadLen};
+
+mod pad_eq;
+mod pad_len;

0e327c094e69308282e34e58d7e431cf50f41b8f
NEXZ <76069669+soynexz@users.noreply.github.com>
2024-07-10 17:08:54 +0200

    Delete all code again

--- crates/commonlib/src/misc/mod.rs
+++
@@ -1,4 +0,0 @@
-// Copyright (c) Toolbi Software. All rights reserved.
-// Check the README file in the project root for more information.
-
-pub use commonlib_macros::misc_variant as variant;
--- crates/commonlib/src/time/mod.rs
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Toolbi Software. All rights reserved.
-// Check the README file in the project root for more information.
-
-pub use self::convert::{
-  TimeConvert, TimeConvertError, TimeConvertUnit, TimeConvertUnitKind, TimeConvertValueKind,
-};
-
-pub mod convert;

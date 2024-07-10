e226ee2fb98e3093eead4e6641bd32ff84981864
NEXZ <76069669+soynexz@users.noreply.github.com>
2024-07-10 23:34:56 +0200

    Update lib.rs

--- crates/commonlib/src/lib.rs
+++ crates/commonlib/src/lib.rs
@@ -2,5 +2,8 @@
 // Check the README file in the project root for more information.
 
 pub use self::error::Error;
+pub use self::str::{pad_eq, pad_len, trunc};
 
 pub mod error;
+pub mod prelude;
+pub mod str;

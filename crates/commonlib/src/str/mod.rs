bfa2402ab0447f144ffd52b9adf400d02ec04833
NEXZ <76069669+soynexz@users.noreply.github.com>
2024-07-11 17:06:42 +0200

    Update mod.rs

--- crates/commonlib/src/str/mod.rs
+++ crates/commonlib/src/str/mod.rs
@@ -1,9 +1,9 @@
 // Copyright (c) Toolbi Software. All rights reserved.
 // Check the README file in the project root for more information.
 
-pub use pad_eq::{pad_eq, PadEq};
-pub use pad_len::{pad_len, PadLen};
-pub use trunc::{trunc, Trunc};
+pub use self::pad_eq::{pad_eq, PadEq};
+pub use self::pad_len::{pad_len, PadLen};
+pub use self::trunc::{trunc, Trunc};
 
 pub mod pad_eq;
 pub mod pad_len;

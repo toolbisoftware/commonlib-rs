14f0e8668c4ecae893baa315b27b59ced238e04e
NEXZ <76069669+soynexz@users.noreply.github.com>
2024-07-16 16:04:19 +0200

    Update lib.rs

--- crates/commonlib/src/lib.rs
+++ crates/commonlib/src/lib.rs
@@ -2,12 +2,14 @@
 // Check the README file in the project root for more information.
 
 pub use self::error::{error, Error};
+pub use self::misc::as_variant;
 pub use self::str::{pad_eq, pad_len, trunc};
 pub use self::time::{Stopwatch, Time, TimeUnit, TimeValue, TimeValueKind};
 
 pub mod error;
 #[cfg(feature = "logger")]
 pub mod logger;
+pub mod misc;
 pub mod num;
 pub mod prelude;
 pub mod str;

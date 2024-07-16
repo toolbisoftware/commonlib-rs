32dd04b4c861dcb54b37fb25ac177972459fff75
NEXZ <76069669+soynexz@users.noreply.github.com>
2024-07-16 16:16:01 +0200

    Update lib.rs

--- crates/commonlib/src/lib.rs
+++ crates/commonlib/src/lib.rs
@@ -1,7 +1,7 @@
 // Copyright (c) Toolbi Software. All rights reserved.
 // Check the README file in the project root for more information.
 
-pub use self::error::{error, Error};
+pub use self::error::{error, errorf, Error};
 pub use self::misc::as_variant;
 pub use self::str::{pad_eq, pad_len, trunc};
 pub use self::time::{Stopwatch, Time, TimeUnit, TimeValue, TimeValueKind};
